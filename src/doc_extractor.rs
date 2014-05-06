7d873e426adab29ff7ce8984a5aa42570d5c1049
Andelf <andelf@gmail.com>
2014-05-06 21:46:01 +0800

    add more dump

--- src/doc_extractor.rs
+++ src/doc_extractor.rs
@@ -12,23 +12,31 @@ use rustdoc::plugins::{PluginCallback, PluginResult, PluginJson};
 // rustdoc -L. --plugin-path . --plugins dummy rust-sdl2/src/sdl2/lib.rs
 // rustdoc --plugin-path . --plugins doc_extractor ~/Repos/rust/src/libcollections/lib.rs
 
+fn dump_item_enum(item: &clean::ItemEnum) {
+    match *item {
+        clean::ModuleItem(ref m) => {
+            println!("|| items => {:?}", m.items);
+        },
+        _ => ()
+    }
+}
+
+fn dump_item(item: &clean::Item) {
+    println!("| item name => {:?}", item.name);
+    println!("| vis => {:?}", item.visibility);
+    for attr in item.attrs.iter() {
+        println!("|   attr => {:?}", attr);
+    }
+    println!("| inner => {:?}", item.inner);
+    dump_item_enum(&item.inner);
+}
+
 #[no_mangle]
 pub fn rustdoc_plugin_entrypoint(c: clean::Crate) -> PluginResult {
     println!("loading extension ok!");
     println!("crate => {}", c.name);
 
-    match c.module {
-        Some(ref item) => {
-            println!("| item name => {:?}", item.name);
-            println!("| vis => {:?}", item.visibility);
-            for attr in item.attrs.iter() {
-                println!("| attr => {:?}", attr);
-            }
-        },
-        _ => ()
-
-    }
-
+    c.module.as_ref().map(dump_item);
     // externs is useless for our app
     // for ext in c.externs.iter() {
     //     println!("externs => {:?}", *ext);
